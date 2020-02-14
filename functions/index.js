872db1f23193ab44947959d69c965ea00da73092
Casneil Simpson <casneil.simpson@gmail.com>
2020-02-14 20:53:42 +0100

    setting up authentification via firebase

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,24 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+// Firebase
+const firebase = require("firebase");
+const firebaseConfig = {
+  apiKey: "AIzaSyAg6iQzBiMhnRsC-VjpmcjTuBEvU0tHBdc",
+  authDomain: "jamsocial-a2a87.firebaseapp.com",
+  databaseURL: "https://jamsocial-a2a87.firebaseio.com",
+  projectId: "jamsocial-a2a87",
+  storageBucket: "jamsocial-a2a87.appspot.com",
+  messagingSenderId: "1044093059363",
+  appId: "1:1044093059363:web:23c84661c267793a328c96",
+  measurementId: "G-PBQQ7SFTQ7"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Express
 const express = require("express");
 const app = express();
 
-// exports.getShouts = functions.https.onRequest((request, response) => {
-
-// });
 app.get("/shouts", (request, response) => {
   admin
     .firestore()
@@ -50,4 +62,27 @@ app.post("/shout", (request, response) => {
     });
 });
 
-exports.api = functions.https.onRequest(app);
+// Signip
+app.post("/signup", (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    name: request.body.name
+  };
+  // validation
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return response
+        .status(201)
+        .json({ message: `User ${data.user.uid} signed up successfully` });
+    })
+    .catch(error => {
+      console.log(error);
+      return response.status(500).json({ error: error.code });
+    });
+});
+
+exports.api = functions.region("europe-west2").https.onRequest(app);
