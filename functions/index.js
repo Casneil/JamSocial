7c7e3ae08b714f7355e0ebbb2a7ac96e6607fd0d
Casneil Simpson <casneil.simpson@gmail.com>
2020-02-14 20:22:51 +0100

    formatting data from firebase to my liking

--- functions/index.js
+++ functions/index.js
@@ -2,40 +2,39 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
 
-exports.getShouts = functions.https.onRequest((request, response) => {
-  // Function URL (getShouts): https://us-central1-jamsocial-a2a87.cloudfunctions.net/getShouts
+const express = require("express");
+const app = express();
+
+// exports.getShouts = functions.https.onRequest((request, response) => {
+
+// });
+app.get("/shouts", (request, response) => {
   admin
     .firestore()
     .collection("shouts")
+    .orderBy("shoutedAt", "desc")
     .get()
     .then(data => {
       let shouts = [];
       data.forEach(doc => {
-        shouts.push(doc.data());
+        shouts.push({
+          shoutId: doc.id,
+          body: doc.data().body,
+          userSubmit: doc.data().body.userSubmit,
+          shoutedAt: doc.data().shoutedAt
+        });
       });
       return response.json(shouts);
     })
     .catch(error => console.log(error));
 });
 
-exports.createShout = functions.https.onRequest((request, response) => {
-  // Function URL (getShouts): https://us-central1-jamsocial-a2a87.cloudfunctions.net/createShout
-  if (request.method !== "Post") {
-    return response
-      .status(400)
-      .json({ message: "Method not allowed for this route" });
-  }
+app.post("/shout", (request, response) => {
   const newShout = {
     body: request.body.body,
     userSubmit: request.body.userSubmit,
-    shoutedAt: admin.firestore.Timestamp.fromDate(new Date())
+    shoutedAt: new Date().toISOString()
   };
 
   admin
@@ -50,3 +49,5 @@ exports.createShout = functions.https.onRequest((request, response) => {
       console.log(error);
     });
 });
+
+exports.api = functions.https.onRequest(app);
