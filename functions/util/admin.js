d4b10e8236f34cf6555f44f5c7276f391cc7059c
Casneil Simpson <casneil.simpson@gmail.com>
2020-02-16 10:51:42 +0100

    finish user routes

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,6 +1,10 @@
 const admin = require("firebase-admin");
+var serviceAccount = require("path/to/serviceAccountKey.json");
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://jamsocial-a2a87.firebaseio.com"
+});
 
 const db = admin.firestore();
 
